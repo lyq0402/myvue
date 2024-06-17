004ea938dbadb5407b1326bb012203abeac88997
Your Name <you@example.com>
2024-06-18 01:23:40 +0800

    api创建

--- src/pages/api-create.vue
+++ src/pages/api-create.vue
@@ -72,27 +72,23 @@
               <el-input style="margin-left: 20px; width: 500px;" v-model="apiForm.name"
                         placeholder="请输入简洁、清晰的API名称"
               @blur="checkAPIName"></el-input>
-              <i v-if="APINameExisted && everChecked" class="el-icon-check" style="color: green; font-size: 20px;"></i>
-              <i v-if="!APINameExisted && everChecked" class="el-icon-close" style="color: red; font-size: 20px;"></i>
+              <i v-if="!APINameExisted && APIEverChecked" class="el-icon-check" style="color: green; font-size: 20px;"></i>
+              <i v-if="APINameExisted && APIEverChecked" class="el-icon-close" style="color: red; font-size: 14px;">该API名称已存在或者不合法</i>
             </el-form-item>
             <el-form-item label="API描述:">
-              <el-input type="textarea" v-model="apiForm.desc"
+              <el-input type="textarea" v-model="apiForm.desc" :rows="6"
                         placeholder="请输入API的描述，用于详细说明API的功能、参数、返回结果等信息。这个描述可以帮助其他开发者理解和正确使用API。"
-                        style="margin-left: 20px; width: 500px; height: 60px;"></el-input>
-            </el-form-item>
-            <el-form-item label="API描述:">
-              <el-input type="textarea" v-model="apiForm.sql" :rows="6"
-                        placeholder="请输入具体的SQL语言,对数据库的操作包括查看，返回修改数据等"
                         style="margin-left: 20px; width: 500px; height: 140px;"></el-input>
             </el-form-item>
-            <el-form-item label="请求方式:">
-              <el-select v-model="apiForm.requestMethod" placeholder="请选择请求方式" style="margin-left: 20px">
-                <el-option label="get" value="get"></el-option>
-                <el-option label="post" value="post"></el-option>
-                <el-option label="delete" value="delete"></el-option>
-                <el-option label="put" value="put"></el-option>
-              </el-select>
+            <el-form-item label="SQL语言:">
+              <el-input type="textarea" v-model="apiForm.sql" :rows="6"
+                        placeholder="请输入具体的SQL语言,对数据库的操作包括查看，返回修改数据等"
+                        @blur="checkSQL"
+                        style="margin-left: 20px; width: 500px; height: 140px;"></el-input>
+              <i v-if="!SQLLegal && SQLEverChecked " class="el-icon-check" style="color: green; font-size: 20px;"></i>
+              <i v-if="SQLLegal && SQLEverChecked " class="el-icon-close" style="color: red; font-size: 14px;">您输入的SQL语言不合法</i>
             </el-form-item>
+
             <el-form-item label="数据库类型:">
               <el-select v-model="apiForm.databaseType" placeholder="请选择数据库类型" style="margin-left: 20px">
                   <el-option
@@ -103,20 +99,12 @@
                   </el-option>
                 </el-select>
             </el-form-item>
-            <el-form-item label="请求头参数（可选）:">
-              <el-input style="margin-left: 20px; width: 500px;" v-model="apiForm.requestHeader"
-                        placeholder="输入API的请求头参数，例如认证信息、内容类型等" ></el-input>
-            </el-form-item>
-            <el-form-item label="身份验证信息（可选）:">
-              <el-input style="margin-left: 20px; width: 500px;" v-model="apiForm.authentication"
-                        placeholder="输入身份验证信息，例如用户名、密码、API密钥等" ></el-input>
-            </el-form-item>
             <el-form-item label="超时终止（可选）:">
               <el-input-number style="margin-left: 20px;" v-model="apiForm.timeout"
                                :min="0" :max="300" :step="1" step-strictly></el-input-number> ms
             </el-form-item>
             <el-form-item label="业务用户:">
-              <el-select style="margin-left: 20px" v-model="user" multiple placeholder="请选择">
+              <el-select style="margin-left: 20px" v-model="apiForm.user" multiple placeholder="请选择">
                 <el-option
                     v-for="item in userList"
                     :key="item.value"
@@ -128,7 +116,7 @@
 
             <el-form-item style="margin-left: 15% ">
               <el-button type="primary" @click="onSubmit">立即创建</el-button>
-              <el-button type="danger" @click="onSubmit">取消</el-button>
+              <el-button type="danger" @click="Reset">取消</el-button>
             </el-form-item>
           </el-form>
         </el-main>
@@ -148,18 +136,15 @@ export default {
   data(){
     return{
       APINameExisted: false,
-      everChecked: false,
+      SQLLegal: false,
+      APIEverChecked: false,
+      SQLEverChecked: false,
       isCollapse:false,
       asideWidth: '200px',
       apiForm: {
         sql: '',
         name: '',
-        requestMethod: '',
         databaseType: '',
-        userid: '',
-        url: '',
-        requestHeader: '',
-        authentication: '',
         timeout: 0,
         user: '',
         desc: ''
@@ -187,75 +172,55 @@ export default {
         {
           value: 'mysql',
           label: 'mysql'
-        },
-        {
-          value: 'oracle',
-          label: 'oracle'
-        },
-        {
-          value: 'sqlserver',
-          label: 'sqlserver'
-        },
-        {
-          value: 'postgresql',
-          label: 'postgresql'
-        },
-        {
-          value: 'mongodb',
-          label: 'mongodb'
-        },
-        {
-          value: 'redis',
-          label: 'redis'
-        },
-        {
-          value: 'elasticsearch',
-          label: 'elasticsearch'
-        },
-        {
-          value: 'hive',
-          label: 'hive'
-        },
-        {
-          value: 'hbase',
-          label: 'hbase'
-        },
-        {
-          value: 'kafka',
-          label: 'kafka'
-        },
-        {
-          value: 'flink',
-          label: 'flink'
-        },
-        {
-          value: 'spark',
-          label: 'spark'
-        },
-        {
-          value: 'hadoop',
-          label: 'hadoop'
-        },
-        {
-          value: 'hive',
-          label: 'hive'
-        },
-        {
-          value: 'hbase',
-          label: 'hbase'
-        },
-        {
-          value: 'kafka',
-          label: 'kafka'
         }]
     }
   },
   methods:{
     onSubmit() {
-      console.log('submit!');
+      let judge = false;
+      let url = '';
+      axios({
+        method: 'post',
+        url: 'http://localhost:10010/api/create',
+        data: {
+          name: this.apiForm.name,
+          info: this.apiForm.desc,
+          command: this.apiForm.sql,
+          business: this.apiForm.user
+        }
+      }).then(response => {
+        judge = response.data.judge
+        url = response.data.url
+        if(judge){
+          this.$alert('您创建的API的URL为${url}，详情请查看“API详细信息查询”页面', '创建API成功', {
+            confirmButtonText: '确定',
+            callback: action => {
+              this.$message({
+                type: 'info',
+                message: `action: ${ action }`
+              });
+            }
+          });
+          this.Reset()
+        }
+        else{
+          this.$alert('创建API失败，请检查您的输入是否合法', '创建API失败', {
+            confirmButtonText: '确定',
+            callback: action => {
+              this.$message({
+                type: 'info',
+                message: `action: ${ action }`
+                });
+            }
+          });
+        }
+      }).catch(error => {
+        console.log(error);
+      })
+
     },
     checkAPIName(){
-      this.everChecked = true;
+      this.APIEverChecked = true;
       axios({
         method: 'post',
         url: 'http://localhost:10010/api/checkname',
@@ -264,10 +229,39 @@ export default {
         }
       }).then(response => {
         this.APINameExisted = response.data
-
       }).catch(error => {
         console.log(error);
       })
+
+      if(this.apiForm.name.trim() === ''){
+        this.APINameExisted = true
+      }
+    },
+    checkSQL(){
+      this.SQLEverChecked = true;
+      axios({
+        method: 'post',
+        url: 'http://localhost:10010/api/check',
+        data: {
+          sql: this.apiForm.sql
+        }
+      }).then(response => {
+        this.SQLLegal = response.data
+      }).catch(error => {
+        console.log(error);
+      })
+    },
+    Reset(){
+      this.apiForm.sql = '';
+      this.apiForm.name = '';
+      this.apiForm.databaseType = '';
+      this.apiForm.url = '';
+      this.apiForm.authentication = '';
+      this.apiForm.timeout = 0;
+      this.apiForm.user = [];
+      this.apiForm.desc = '';
+      this.APIEverChecked = false;
+      this.SQLEverChecked = false;
     }
   }
 }
